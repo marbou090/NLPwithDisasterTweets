b5bc0c478da30e3503ad129613e996d81129739d
marubou090 <rikka2217@gmail.com>
2021-02-01 10:47:00 +0900

    テキストの空白区切り雛形

--- DataCleaning.py
+++ DataCleaning.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd 
 
 #%%
-train = pd.read_csv("train.csv")
+train = pd.read_csv("data/train.csv")
 train.head()
 
 #%%
@@ -18,4 +18,14 @@ train.describe()
 train.describe(include=['O'])
 # %%
 train.isnull().sum()
+# %%
+
+# %%
+sample = train.iloc[0]
+#%%
+target_word = pd.DataFrame()
+target_word = pd.concat([target_word,pd.Series( sample['text'].split())], ignore_index=True)
+
+
+
 # %%
